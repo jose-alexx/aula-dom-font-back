48af017a2f8cbae0c532dbe66168d4b8397570cb
jose alex <josealex41012@gmail.com>
2024-11-21 11:19:40 -0300

    btn-voltar

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ ul.appendChild(li);
 */
 
 const btnVerificar = document.querySelector("#btn-verificar");
+const btnVoltar = document.querySelector("#btn-voltar");
 // console.log(btnVerificar);
 
 btnVerificar.addEventListener('click', () => {
@@ -26,13 +27,36 @@ btnVerificar.addEventListener('click', () => {
     const numero = Number(valor.value);
 
     const h2 = document.querySelector("h2");
-    
+
     if (numero % 2 == 0) 
-        console.log("Par");
-     else 
-        console.log("Impar");
+        h2.innerText = `O número ${numero} é par`;
+        // console.log("Par");
+    else 
+        h2.innerText = `O número ${numero} é impar`;
+        // console.log("Impar");
     
+    const resultado = document.querySelector("#resultado");
+
+    resultado.classList.remove("hide");
+
+    const main = document.querySelector("#main")
+
+    main.classList.add("hide")
+
     valor.valor = "";
 });
+
+btnVoltar.addEventListener('click', () => {
+    console.log("Clicou!");
+    
+    const resultado = document.querySelector("#resultado");
+
+    resultado.classList.add("hide");
+
+    const main = document.querySelector("#main")
+
+    main.classList.remove("hide")
+
+})
 
 
